3bcb64c04ec83861502549d0f2b7baae89d59fce
Daniel Tinoco <w.dniel303@hotmail.com>
2022-03-31 09:10:00 -0600

    [U] rules eslint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,14 +7,14 @@ const db = admin.firestore();
 
 export const refreshData = functions.https.onRequest((request, response) => {
   const collectionsAllowed = [
-    { name: 'ticker_all', id: 'kayjx9kZE6JRpJ5WZCgw' },
-    { name: 'ticker_all_staging', id: 'Qnmm6ZmqgI4vRb1QIMCR' },
-    { name: 'market_summary', id: 'PwerdYXavQnUsuk7rCKS' },
-    { name: 'market_summary_staging', id: 'ewMsAwtJY5LzvzzunvID' },
+    {name: "ticker_all", id: "kayjx9kZE6JRpJ5WZCgw"},
+    {name: "ticker_all_staging", id: "Qnmm6ZmqgI4vRb1QIMCR"},
+    {name: "market_summary", id: "PwerdYXavQnUsuk7rCKS"},
+    {name: "market_summary_staging", id: "ewMsAwtJY5LzvzzunvID"},
   ];
-  const collection = collectionsAllowed.find(collection => collection.name === request.body.collection);
+  const collection = collectionsAllowed.find((collection) => collection.name === request.body.collection);
   if (!collection) {
-    response.status(404).json({ message: 'Not found collection ' + request.body.collection })
+    response.status(404).json({message: "Not found collection " + request.body.collection});
   } else {
     db.collection(collection.name).doc(collection.id).update({
       data: request.body.data,
