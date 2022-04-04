c03218d91f5836e0bc945e13d8aae68da6eff3d0
Daniel Tinoco <w.dniel303@hotmail.com>
2022-04-04 07:39:06 -0500

    [U] new structure

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,6 +6,10 @@ admin.firestore().settings({timestampsInSnapshots: true});
 const db = admin.firestore();
 
 export const refreshData = functions.https.onRequest(async (request, response) => {
+  functions.logger.info("Created or updated data", {
+    structuredData: true,
+    body: request.body,
+  });
   const collectionsAllowed = [
     "ticker_all",
     "ticker_all_staging",
@@ -17,7 +21,7 @@ export const refreshData = functions.https.onRequest(async (request, response) =
     response.status(404).json({message: "Not found collection " + request.body.collection});
   } else {
     const data = request.body.data;
-    if (data && data.length) for (const item of data) await db.collection(collection).doc(item.symbol.replace("/", "_")).set(item);
+    if (data && data.length) await db.collection(collection).doc('info').set({data});
     response.status(200).json({message: "Updated or created data"});
   }
 });
